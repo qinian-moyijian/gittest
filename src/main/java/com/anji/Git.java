f14b65a59913302cdc496ff168652e74a56f32e2
zhangxiaoming <zhangxiaoming@anji-plus.com>
2021-01-28 16:52:27 +0800

    第一次提交

---
+++ src/main/java/com/anji/Git.java
@@ -0,0 +1,12 @@
+package com.anji;
+
+/**
+ * \* Date: 2021/1/28
+ * \*
+ * \* Desc:
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
